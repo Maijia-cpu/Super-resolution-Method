338f7bd8a8b543f5d88dca834ca906ecade3f7a4
Maijia-cpu <63754415+Maijia-cpu@users.noreply.github.com>
2021-02-19 16:09:29 -0500

    Update create_image.m

--- create_image.m
+++ create_image.m
@@ -10,7 +10,6 @@ radius=param.radius;%9nm/pixel
 circlePixels=(rowsInImage-centerX).^2+(columnsInImage-centerZ).^2<=radius.^2;
 circlePixels_sub=(rowsInImage-centerX).^2+(columnsInImage-centerZ).^2<=(radius-1).^2;
 I=circlePixels-circlePixels_sub;
-% [nz nx]=size(I(:,:));
-% I_new=imresize(I, [round(nz/2.78) round(nx)],'bilinear'); % z res 25nm/pixel x,y res 9nm/pixel
+
 
 end
